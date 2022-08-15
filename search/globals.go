840b05cc455cd73f2a1ce291f45892f5ae760721
DeybisMelendez <pixelweb2007@gmail.com>
2022-08-15 11:49:06 -0600

    Proyecto base

--- search/globals.go
+++ search/globals.go
@@ -1,21 +1,16 @@
 package search
 
-import (
-	chess "github.com/dylhunn/dragontoothmg"
-)
-
-const MaxPly = 64
+import "nicarao/eval"
 
 var Ply int = 0
 var Nodes int = 0
+
+const MaxPly = 64
 
 //var Mate int = 4000
 
 func ResetGlobalVariables() {
 	Ply = 0
 	Nodes = 0
-}
-
-func GetPieces(move chess.Move, board *chess.Board) {
-	println(board.White.All)
+	eval.ResetMaterial()
 }
