6b3c3f11c906faaf076daed9ab8f915f30262e58
DeybisMelendez <pixelweb2007@gmail.com>
2022-08-22 14:30:47 -0600

    desactivado evaluacion de peones

--- main.go
+++ main.go
@@ -4,20 +4,7 @@ import (
 	"nicarao/uci"
 )
 
-/*
-const infinity int = 5000
-
-var depth int = 6
-var color int = 1
-*/
 func main() {
 	uci.Init()
 	uci.UCI()
-	/*start := time.Now()
-	board := chess.ParseFen(startpos)
-	search.InitHasTable()
-	search.ResetGlobalVariables()
-	score := search.Negascout(&board, depth, color, -infinity, infinity)
-	elapsed := time.Since(start)
-	fmt.Println("Score:", score, "Nodes:", search.Nodes, "Time:", elapsed)*/
 }
