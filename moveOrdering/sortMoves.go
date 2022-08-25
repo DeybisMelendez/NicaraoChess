b81eaaa3b392992386d3e805b6a0e35d72da0f6d
DeybisMelendez <pixelweb2007@gmail.com>
2022-08-25 15:38:03 -0600

    pequeña mejora en sortmoves

--- moveOrdering/sortMoves.go
+++ moveOrdering/sortMoves.go
@@ -10,7 +10,7 @@ import (
 var FollowPV bool
 
 func valueMove(board *chess.Board, move chess.Move, pvMove chess.Move, bestmove chess.Move, isWhite bool, ply int) int {
-	score := 0
+	score := GetMVV_LVA(move, board)
 	piece, _ := utils.GetPiece(move.From(), board)
 	historyMove := GetHistoryMove(isWhite, piece, move.To())
 	if move == bestmove {
@@ -22,13 +22,13 @@ func valueMove(board *chess.Board, move chess.Move, pvMove chess.Move, bestmove 
 			score = 4000
 		}
 	} else if chess.IsCapture(move, board) {
-		score = GetMVV_LVA(move, board) + 3000
+		score += 3000
 	} else if KillerMoves[0][ply] == move {
 		score = 2000
 	} else if KillerMoves[1][ply] == move {
 		score = 1000
 	} else {
-		score = historyMove
+		score += historyMove
 	}
 	return score
 }
