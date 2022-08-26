fc9bbe2beb9965d4c1123956853a03c6b57df041
DeybisMelendez <pixelweb2007@gmail.com>
2022-08-26 16:34:12 -0600

    Cambiando de sorting a Quicksort, mas eficiente

--- moveOrdering/sortMoves.go
+++ moveOrdering/sortMoves.go
@@ -1,8 +1,8 @@
 package moveOrdering
 
 import (
+	"math/rand"
 	"nicarao/utils"
-	"sort"
 
 	chess "github.com/dylhunn/dragontoothmg"
 )
@@ -27,9 +27,47 @@ func valueMove(board *chess.Board, move chess.Move, pvMove chess.Move, bestmove 
 }
 
 func SortMoves(moves []chess.Move, board *chess.Board, pvMove chess.Move, bestmove chess.Move, ply int) {
-	sort.Slice(moves, func(a, b int) bool {
-		valueA := valueMove(board, moves[a], pvMove, bestmove, board.Wtomove, ply)
-		valueB := valueMove(board, moves[b], pvMove, bestmove, board.Wtomove, ply)
-		return valueA > valueB
-	})
+	var list = make([]int, len(moves))
+	for i, m := range moves {
+		list[i] = valueMove(board, m, pvMove, bestmove, board.Wtomove, ply)
+	}
+	Quicksort(list, moves)
 }
+func Quicksort(a []int, moves []chess.Move) {
+	if len(a) < 2 {
+		return
+	}
+	left, right := 0, len(a)-1
+
+	pivot := rand.Int() % len(a)
+
+	a[pivot], a[right] = a[right], a[pivot]
+	moves[pivot], moves[right] = moves[right], moves[pivot]
+	for i := range a {
+		if a[i] > a[right] {
+			a[left], a[i] = a[i], a[left]
+			moves[left], moves[i] = moves[i], moves[left]
+			left++
+		}
+	}
+
+	a[left], a[right] = a[right], a[left]
+	moves[left], moves[right] = moves[right], moves[left]
+	Quicksort(a[:left], moves[:left])
+	Quicksort(a[left+1:], moves[left+1:])
+
+	//return a, moves
+}
+
+/*func SelectionSort(items []chess.Move) {
+	var n = len(items)
+	for i := 0; i < n; i++ {
+		var minIdx = i
+		for j := i; j < n; j++ {
+			if items[j] < items[minIdx] {
+				minIdx = j
+			}
+		}
+		items[i], items[minIdx] = items[minIdx], items[i]
+	}
+}*/
